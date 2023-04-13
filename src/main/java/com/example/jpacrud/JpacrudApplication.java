3f4a71a28b1b982ced2cbe5234d94106fe5af5ca
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2023-04-13 16:19:08 +0900

    first commit

---
+++ src/main/java/com/example/jpacrud/JpacrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpacrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpacrudApplication.class, args);
+    }
+
+}
